8895ea36340b221329e26660248f623f6e55ec89
Matt Maginniss <matthew.maginniss@gmail.com>
2022-10-11 22:23:30 +0200

    Implemented palindrome checking

--- classic-problems/src/main.rs
+++ classic-problems/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    println!("\nFizz Buzz");
     fizz_buzz();
+    println!("\nPalindromes");
+    palindrome_checker();
 }
 
 fn fizz_buzz() {
@@ -18,4 +21,22 @@ fn fizz_buzz() {
         }
         number += 1;
     }
+}
+
+fn palindrome_checker() {
+    println!("{}", is_palindrome("word"));
+    println!("{}", is_palindrome("racecar"));
+    println!("{}", is_palindrome("RaCEcAr"));
+    println!("{}", is_palindrome("poopy"));
+    println!("{}", is_palindrome("gary"));
+    println!("{}", is_palindrome("poop"));
+}
+
+fn is_palindrome(word: &str) -> bool {
+    println!(
+        "{0} - {1}",
+        word.to_lowercase(),
+        word.to_lowercase().chars().rev().collect::<String>()
+    );
+    word.to_lowercase() == word.to_lowercase().chars().rev().collect::<String>()
 }
